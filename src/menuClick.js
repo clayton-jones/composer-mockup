d0cc27720918e7048a1708a31b1a2a919202db86
Clayton Jones <claytonjonesLMP@gmail.com>
2020-06-17 20:52:24 -0700

    removed unused variables preventing build compile

--- src/menuClick.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-let hamburger = document.getElementById('hamburger');
-let navContainer = document.getElementById('nav-container');
-hamburger.addEventListener('click', () => {
-  console.log('click');
-  navContainer.classList.toggle('show');
-});
